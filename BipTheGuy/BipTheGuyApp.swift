7c2e56bb59a549177e94d6362f9d6a022998c591
Franklyn <franklynahilon@gmail.com>
2026-04-09 11:26:32 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by AHILON CANO, FRANKLYN on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
