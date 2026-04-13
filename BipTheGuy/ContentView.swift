6aed6747c4ecb5f8e3502e25489f9c9b5e9c1f90
Franklyn <franklynahilon@gmail.com>
2026-04-13 13:10:19 -0700

    na

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -13,11 +13,12 @@ struct ContentView: View {
     @State private var audioPlayer : AVAudioPlayer!
     @State private var isFullSize = true
     @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     var body: some View {
         VStack {
             Spacer()
             
-          Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit( )
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -32,11 +33,20 @@ struct ContentView: View {
             
             Spacer()
             
-            PhotosPicker(selection: $selectedPhoto) {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
 
             }
-           
+            .onChange(of: selectedPhoto) {
+                
+                Task  {
+                    guard let selectedPhoto = try? await selectedPhoto?.loadTransferable(type: Image.self)else {
+                        print("😡 Could not get image from loadTransferrable.")
+                        return
+                    }
+                    bipImage = selectedPhoto
+                }
+            }
 
         }
         .padding()
